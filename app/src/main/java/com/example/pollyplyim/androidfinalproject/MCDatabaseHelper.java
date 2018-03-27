6a7dcdaecd73579330008045828162de8de0b124
pollypolingyim <>
2018-03-26 21:48:19 -0400

    2nd commit

--- app/src/main/java/com/example/pollyplyim/androidfinalproject/MCDatabaseHelper.java
+++ app/src/main/java/com/example/pollyplyim/androidfinalproject/MCDatabaseHelper.java
@@ -15,10 +15,17 @@ public class MCDatabaseHelper extends SQLiteOpenHelper {
     static final int VERSION_NUM = 1; //database version
     static final String KEY_ID = "ID"; //column name, primary key
     static final String KEY_QUESTION = "QuestionCol"; //column name
-    static final String KEY_CHOICE = "ChoiceCol";
-    static final String KEY_USER_ANS = "UserAnsCol";
-    static final String KEY_CORR_ANS = "CorrAnsCol";
-    static final String TB_NAME = "MessageTb"; //table name
+    static final String KEY_CHOICE_A = "ChoiceACol";
+    static final String KEY_CHOICE_B = "ChoiceBCol";
+    static final String KEY_CHOICE_C = "ChoiceCCol";
+    static final String KEY_CHOICE_D = "ChoiceDCol";
+    static final String KEY_CORRECT_CHOICE = "CorrChoiceCol";
+    static final String KEY_CORRECT_TF = "CorrTFCol";
+    static final String KEY_USER_TEXT_INPUT = "UserTextInputCol";
+    static final String KEY_CORRECT_ANS_TEXT = "CorrTextCol";
+    static final String KEY_USER_NUM_INPUT = "UserNumInputCol";
+    static final String KEY_CORRECT_ANS_NUM = "CorrNumCol";
+    static final String TB_NAME = "MCTable"; //table name
 
     public MCDatabaseHelper(Context ctx) {
         super(ctx, DATABASE_NAME, null, VERSION_NUM);
@@ -31,9 +38,16 @@ public class MCDatabaseHelper extends SQLiteOpenHelper {
         String CREATE_TABLE = "CREATE TABLE " + TB_NAME + "("
                 + KEY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
                 + KEY_QUESTION + " TEXT,"
-                + KEY_USER_ANS + " TEXT,"
-                + KEY_CORR_ANS + " TEXT,"
-                + KEY_QUESTION + " TEXT);";
+                + KEY_CHOICE_A + " TEXT, "
+                + KEY_CHOICE_B + " TEXT,"
+                + KEY_CHOICE_C + " TEXT,"
+                + KEY_CHOICE_D + " TEXT,"
+                + KEY_CORRECT_CHOICE + " TEXT,"
+                + KEY_CORRECT_TF + " TEXT, "
+                + KEY_USER_TEXT_INPUT + " TEXT,"
+                + KEY_CORRECT_ANS_TEXT + "TEXT,"
+                + KEY_USER_NUM_INPUT + "REAL,"
+                + KEY_CORRECT_ANS_NUM + " REAL);";
         db.execSQL(CREATE_TABLE);
     }
 
--- app/src/main/java/com/example/pollyplyim/androidfinalproject/MultipleChoiceActivity.java
+++ app/src/main/java/com/example/pollyplyim/androidfinalproject/MultipleChoiceActivity.java
@@ -1,44 +1,110 @@
 package com.example.pollyplyim.androidfinalproject;
 
+import android.app.Activity;
 import android.content.ContentValues;
+import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
 
-public class MultipleChoiceActivity extends SingleFragmentActivity {
-    protected static final String ACTIVITY_NAME = "ChatWindowActivity";
+public class MultipleChoiceActivity extends Activity {
+    protected static final String ACTIVITY_NAME = "MultipleChoiceActivity";
     private ListView listView;
-    private EditText editT;
-    private Button sendBut;
-    private ArrayList<String> chatMsg;
-    //private ChatAdapter messageAdapter;
     private MCDatabaseHelper mdh;
     private static SQLiteDatabase sld;
+    private ArrayList<String> rows;
     private ContentValues cv;
     private Cursor cursor;
+    private boolean isChoice;
+    private boolean isTF;
+    private boolean isInput;
+    private  RowAdapter rowAdapter;
+    private Button a;
+    private Button b;
+    private Button c;
+    private Button d;
 
     @Override
-    protected Fragment createFragment(){
-        return new MCListFragment();
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+        listView = (ListView)findViewById(R.id.main_page);
+        mdh = new MCDatabaseHelper(getApplicationContext());
+        sld = mdh.getWritableDatabase();
+        cv = new ContentValues();
+        a = (Button)findViewById(R.id.buttonA);
+        b = (Button)findViewById(R.id.buttonB);
+        c = (Button)findViewById(R.id.buttonC);
+        d = (Button)findViewById(R.id.buttonD);
+
+
+        String str = "select * from " + MCDatabaseHelper.TB_NAME;
+        cursor = sld.rawQuery(str, null);
+
+        cursor.moveToFirst();
+        while(!cursor.isAfterLast() ){
+            String question = cursor.getString(1);
+            rows.add(question);
+            if(cursor.getString(2)!=null) isChoice=true;
+            else if(cursor.getString(7)!=null)isTF=true;
+            else if(cursor.getString(2)==null && cursor.getString(7)==null)
+                isInput=true;
+            cursor.moveToNext();
+        }
+
+        rowAdapter=new RowAdapter( this );
+        listView.setAdapter (rowAdapter);
     }
 
+    private class RowAdapter extends ArrayAdapter<String> {
+        public RowAdapter(Context ctx) {
+            super(ctx, 0);
+        }
+        public int getCount(){
+            return rows.size();
+        }
 
+        public String getItem(int position){
+            return rows.get(position);
+        }
 
+        public View getView(int position, View convertView, ViewGroup parent){
+            LayoutInflater inflater = MultipleChoiceActivity.this.getLayoutInflater();
+            View result = null ;
+            if(isChoice) {
+                result = inflater.inflate(R.layout.choice_layout, null);
+                a.setText(getItem(position));
+                b.setText(getItem(position));
+                c.setText(getItem(position));
+                d.setText(getItem(position));
+            }
+            else if(isTF)
+                result = inflater.inflate(R.layout.true_false_layout, null);
+            else if(isInput)
+                result = inflater.inflate(R.layout.text_input_layout, null);
 
+            return result;
+        }
 
+        public long getId(int position){
+            return position;
+        }
 
-    @Override
-    public void onListItemClick(ListView l, View v, int position, long id) {
-        // Do something when a list item is clicked
+    }
+
+    public void onDestroy(){
+        super.onDestroy();
+        cursor.close();
+        mdh.close();
+        sld.close();
     }
 }
